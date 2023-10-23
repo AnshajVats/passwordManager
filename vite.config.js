0ebcc4f44a55fb18aa17008671bb6c8f149b8df0
Anshaj vats <9931633906vats@gmail.com>
2023-10-23 03:21:41 -0700

    changes for web pages to render

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Set the desired output directory name (e.g., 'build')
-  },
-  base: "/passwordManager/",
+  base: '/passwordManager/'
 })
